a125b20c87d4c82286beb580f2adc4137f428d9e
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-07-03 23:57:41 +0900

    feat:サブディレクトリ13

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-//   base: process.env.VERCEL ? '/d/' : '/',
+  base: '/d/',
+  site: 'https://portfolio.tsukuri-tai.com',
   vite: {
     plugins: [tailwindcss()]
   }
